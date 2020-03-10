2266b82c19b62e2b7398629554685c5a4a6e337e
vlyagusha <vlyagusha@justclick.ru>
2020-03-10 20:15:09 +0300

    отправка email с отчетом

--- src/EventListeners/EmailReportSendSubscriber.php
+++ src/EventListeners/EmailReportSendSubscriber.php
@@ -5,9 +5,24 @@ namespace App\EventListeners;
 use App\Event\ReportEvent;
 use App\Event\ReportEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class EmailReportSendSubscriber implements EventSubscriberInterface
 {
+    private MailerInterface $mailer;
+
+    private array $reportEmailRecipients;
+
+    private string $reportEmailSender;
+
+    public function __construct(MailerInterface $mailer, array $reportEmailRecipients, string $reportEmailSender)
+    {
+        $this->mailer = $mailer;
+        $this->reportEmailRecipients = $reportEmailRecipients;
+        $this->reportEmailSender = $reportEmailSender;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
@@ -17,6 +32,31 @@ class EmailReportSendSubscriber implements EventSubscriberInterface
 
     public function onReportSend(ReportEvent $event): void
     {
+        $reportData = 'Report data is empty!';
 
+        if (!empty($event->getReport())) {
+            $reportData = [];
+            foreach ($event->getReport() as $report) {
+                $reportData[] = implode(';', [
+                    $report->getEpolkaId(),
+                    $report->getConnectedAt()->format('Y-m-d H:i:s'),
+                    $report->getSignalLevel(),
+                    $report->getVoltage(),
+                    implode(';', $report->getSensors())
+                ]);
+            }
+            $reportData = implode("\n", $reportData);
+        }
+
+        $reportEmail = (new Email())
+            ->from($this->reportEmailSender)
+            ->to(...$this->reportEmailRecipients)
+            ->subject('Epolka report')
+            ->text($reportData)
+        ;
+
+        $this->mailer->send($reportEmail);
+
+        return;
     }
 }
