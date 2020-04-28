ca51fda0f5618ad889de2542ebc29c2731877d17
vlyagusha <vlyagusha@justclick.ru>
2020-04-28 21:51:41 +0300

    выбираем в отчет только последнюю запись из всех

--- src/Command/SendReportCommand.php
+++ src/Command/SendReportCommand.php
@@ -39,7 +39,7 @@ class SendReportCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $period = new \DateTime($input->getOption('period'), new \DateTimeZone('Europe/Moscow'));
-        $report = $this->entityManager->getRepository(EpolkaData::class)->getReportData($period);
+        $report = $this->entityManager->getRepository(EpolkaData::class)->getUniqueReportData($period);
 
         $this->eventDispatcher->dispatch(new ReportEvent($report), ReportEvents::ON_SEND);
 
--- src/Repository/EpolkaDataRepository.php
+++ src/Repository/EpolkaDataRepository.php
@@ -1,10 +1,11 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Repository;
 
 use App\Entity\EpolkaData;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 /**
  * @method EpolkaData|null find($id, $lockMode = null, $lockVersion = null)
@@ -27,5 +28,30 @@ class EpolkaDataRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult()
         ;
+    }
+
+    public function getUniqueReportData(\DateTime $period): array
+    {
+        $rsm = new ResultSetMapping();
+        $rsm->addEntityResult(EpolkaData::class, 'epolka_data');
+        $rsm->addFieldResult('epolka_data', 'id', 'id');
+        $rsm->addFieldResult('epolka_data', 'epolka_id', 'epolkaId');
+        $rsm->addFieldResult('epolka_data', 'connected_at', 'connectedAt');
+        $rsm->addFieldResult('epolka_data', 'connect_id', 'connectId');
+        $rsm->addFieldResult('epolka_data', 'voltage', 'voltage');
+        $rsm->addFieldResult('epolka_data', 'signal_level', 'signalLevel');
+        $rsm->addFieldResult('epolka_data', 'sensors', 'sensors');
+
+        return $this->getEntityManager()
+            ->createNativeQuery('
+                select * from epolka_data
+                inner join (
+                select epolka_id, max(connected_at) as connected_at
+                from epolka_data
+                where connected_at >= :connectedAt
+                group by epolka_id ) tmp using (epolka_id, connected_at);
+                ', $rsm)
+            ->setParameter('connectedAt', $period)
+            ->getResult();
     }
 }
