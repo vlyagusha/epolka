4746088443a86dc61d0c5466147200b6321a98fa
vlyagusha <vlyagusha@justclick.ru>
2020-08-18 21:15:29 +0300

    лок версии капистрано

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.13.0"
+lock "~> 3"
 
 set :application, 'ePolka'
 set :repo_url, 'git@github.com:vlyagusha/epolka.git'
